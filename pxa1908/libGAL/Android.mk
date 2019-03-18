029e9d4da1669957c2a06bf93fd6f27483c2dce9
Nemirtingas <nanaki89@hotmail.fr>
2019-03-18 13:40:27 +0100

    gal

--- pxa1908/libGAL/Android.mk
+++ pxa1908/libGAL/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_SRC_FILES := libGAL.so
-LOCAL_EXPORT_C_INCLUDE_DIRS := hardware/marvell/pxa1908/original-kernel-headers/drivers/marvell/graphics/galcore_src/hal/inc
+LOCAL_EXPORT_C_INCLUDE_DIRS := $(LOCAL_PATH)/include
 
 LOCAL_MODULE := libGAL
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
